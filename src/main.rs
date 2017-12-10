51fe3dfe17aa46c9a1c6d8c78591f8881c660eb5
Sam Whited <sam@samwhited.com>
2017-12-10 01:09:52 -0600

    main: move startup logic to activate signal

--- src/main.rs
+++ src/main.rs
@@ -45,46 +45,46 @@ mod widget;
 
 fn main() {
     gtk::init().expect(translate!("failed_to_start_gtk"));
-
     let app = gtk::Application::new(Some(res::APP_ID), gio::ApplicationFlags::FLAGS_NONE).unwrap();
 
     let config = config::load_config();
 
-    let display = gdk::Display::get_default().unwrap();
-    let screen = display.get_default_screen();
-    let style_provider = gtk::CssProvider::new();
-    style_provider.load_from_data(res::STYLE_MAIN).unwrap();
+    app.connect_startup(|_| {});
+    app.connect_activate(move |app| {
+        let display = gdk::Display::get_default().unwrap();
+        let screen = display.get_default_screen();
+        let style_provider = gtk::CssProvider::new();
+        style_provider.load_from_data(res::STYLE_MAIN).unwrap();
 
-    match config.theme.as_ref().map(|s| s.as_ref()) {
-        Some("dark") => {
-            if let Some(settings) = gtk::Settings::get_default() {
-                settings.set_property_gtk_application_prefer_dark_theme(true);
+        match config.theme.as_ref().map(|s| s.as_ref()) {
+            Some("dark") => {
+                if let Some(settings) = gtk::Settings::get_default() {
+                    settings.set_property_gtk_application_prefer_dark_theme(true);
+                }
             }
-        }
-        Some("light") => {
-            if let Some(settings) = gtk::Settings::get_default() {
-                settings.set_property_gtk_application_prefer_dark_theme(false);
+            Some("light") => {
+                if let Some(settings) = gtk::Settings::get_default() {
+                    settings.set_property_gtk_application_prefer_dark_theme(false);
+                }
             }
+            Some("conversations") => {
+                style_provider
+                    .load_from_data(res::STYLE_CONVERSATIONS)
+                    .unwrap();
+                gtk::StyleContext::add_provider_for_screen(
+                    &screen,
+                    &style_provider,
+                    gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+                );
+            }
+            _ => {}
         }
-        Some("conversations") => {
-            style_provider
-                .load_from_data(res::STYLE_CONVERSATIONS)
-                .unwrap();
-            gtk::StyleContext::add_provider_for_screen(
-                &screen,
-                &style_provider,
-                gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-            );
-        }
-        _ => {}
-    }
 
-    let logoloader = gdk_pixbuf::PixbufLoader::new();
-    logoloader.loader_write(res::SVG_LOGO).unwrap();
-    logoloader.close().unwrap();
-    let logobuf = logoloader.get_pixbuf().unwrap();
+        let logoloader = gdk_pixbuf::PixbufLoader::new();
+        logoloader.loader_write(res::SVG_LOGO).unwrap();
+        logoloader.close().unwrap();
+        let logobuf = logoloader.get_pixbuf().unwrap();
 
-    app.connect_startup(move |app| {
         let window = widget::AppWindow::new(&app, &logobuf);
         if config.accounts.len() == 0 {
             let login_pane = widget::Login::new(&logobuf);
@@ -94,7 +94,6 @@ fn main() {
         app.add_window(window.as_ref());
         window.as_ref().show_all();
     });
-    app.connect_activate(|_| {});
 
     if let Err(e) = app.register(None) {
         eprintln!("{}: {}", translate!("error_registering_app"), e);
