234696902678740f5c1a6c833232b444c4f8a3c5
Sam Whited <sam@samwhited.com>
2017-12-14 14:13:18 -0600

    ui: layout roster in grid

--- src/ui/roster.rs
+++ src/ui/roster.rs
@@ -5,8 +5,9 @@ use gdk_pixbuf;
 use gtk;
 use gtk::ContainerExt;
 use gtk::FrameExt;
-use gtk::ListBoxExt;
+use gtk::GridExt;
 use gtk::PanedExt;
+use gtk::StackExt;
 use gtk::StackSidebarExt;
 use gtk::StyleContextExt;
 use gtk::WidgetExt;
@@ -38,9 +39,16 @@ impl Roster {
         frame2.add(&stack);
 
         let scroll = gtk::ScrolledWindow::new(None, None);
-        let list = gtk::ListBox::new();
-        scroll.add(&list);
-        stack.add(&scroll);
+        let grid = gtk::Grid::new();
+        grid.set_row_homogeneous(true);
+        grid.set_column_homogeneous(true);
+        grid.set_row_spacing(12);
+        grid.set_column_spacing(12);
+        grid.insert_column(0);
+        grid.insert_column(1);
+        grid.insert_column(2);
+        scroll.add(&grid);
+        stack.add_titled(&scroll, "All", "All");
 
         let names = vec![
             "Beautiful",
@@ -51,21 +59,27 @@ impl Roster {
             "Mailman",
             "Papa Shrimp",
             "Pockets",
+            "Poison Ivey",
+            "Shrimpette",
             "Spoon Foot",
             "Sunshine",
             "Thespian",
             "Twinkle Toes",
+            "Utah Red",
+            "Wired",
             "Zodiac",
         ];
-        names
-            .iter()
-            .inspect(|name| {
-                let lbr = gtk::ListBoxRow::new();
-                let c = contact(name, None);
-                lbr.add(&c);
-                list.insert(&lbr, -1);
-            })
-            .count();
+        let mut x = 0;
+        let mut y = 0;
+        names.iter().for_each(|name| {
+            let c = contact(name, None);
+            grid.attach(&c, x, y, 1, 1);
+            x += 1;
+            if x % 3 == 0 {
+                y += 1;
+                x = 0;
+            }
+        });
 
         paned.show_all();
 
