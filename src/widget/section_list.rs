fb29b63fd5a1063f07f0dbbb39856f9d24e5cff8
Sam Whited <sam@samwhited.com>
2017-12-20 10:10:59 -0600

    widget: style section list

--- src/widget/section_list.rs
+++ src/widget/section_list.rs
@@ -1,7 +1,13 @@
 use gtk;
 use gtk::ContainerExt;
+use gtk::CssProviderExt;
 use gtk::ListBoxExt;
+use gtk::ListBoxRowExt;
 use gtk::ScrolledWindowExt;
+use gtk::StyleContextExt;
+use gtk::WidgetExt;
+
+use res;
 
 use std::collections::HashMap;
 
@@ -29,10 +35,34 @@ impl<'a> SectionList<'a> {
     }
 
     pub fn add<P: gtk::IsA<gtk::Widget>>(&mut self, s: &'a str, widget: &P) {
+        let row = gtk::ListBoxRow::new();
         let label = gtk::Label::new(s);
-        self.list.insert(&label, -1);
+        let ss = s.to_owned();
+        row.connect_activate(move |_| {
+            eprintln!("Clicked {}", ss);
+        });
+        if let Some(style_context) = label.get_style_context() {
+            style_context.add_class("selectlabel");
+            let provider = gtk::CssProvider::new();
+            match provider.load_from_data(res::STYLE_LIST) {
+                Ok(_) => style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION),
+                Err(err) => eprintln!("error loading style provider for list: {}", err),
+            }
+        }
+        row.add(&label);
+        self.list.insert(&row, -1);
+        let sep = gtk::Separator::new(gtk::Orientation::Horizontal);
+        self.view.add(&sep);
         let label = gtk::Label::new(s);
         self.view.add(&label);
+        if let Some(style_context) = label.get_style_context() {
+            style_context.add_class("title");
+            let provider = gtk::CssProvider::new();
+            match provider.load_from_data(res::STYLE_LIST) {
+                Ok(_) => style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION),
+                Err(err) => eprintln!("error loading style provider for list: {}", err),
+            }
+        }
         self.view.add(widget);
         self.sections.insert(s, self.scroll.get_vadjustment());
     }
