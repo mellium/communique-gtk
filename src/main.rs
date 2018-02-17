e7e001f86f11b67fc21e87b3a7c76d66b402a62b
Sam Whited <sam@samwhited.com>
2018-02-17 10:39:44 -0600

    all: remove unused proc_macro to fix stable build

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@
 //! - `win.search` — toggle the search bar
 
 #![crate_type = "bin"]
-#![feature(proc_macro)]
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "https://www.rust-lang.org/favicon.ico", html_root_url = "/")]
 
